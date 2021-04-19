35c10bc7702be99051053a3aa815c12db4f9aa82
Abdul Rehman <mehars.6925@gmail.com>
2021-04-20 02:09:17 +0500

    initialized the plugin

---
+++ mobjaan-plugin.php
@@ -0,0 +1,13 @@
+/**
+ * @package Mobjaan
+ */
+/*
+Plugin Name: Mobjaan Plugin
+Plugin URI: https://github.com/mobjaan
+Description: Listing plugin
+Version: 1.0.0
+Author: Abdul Rehman
+Author URI: https://github.com/AbdulRehmanMehar
+License: GPLv2 or later
+Text Domain: mobjaan-plugin
+*/
