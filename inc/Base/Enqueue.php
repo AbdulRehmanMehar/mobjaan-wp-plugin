0f1a48ff892e3dbdb6753b3b25befa52a69a00a1
Abdul Rehman <mehars.6925@gmail.com>
2021-04-29 01:22:41 +0500

    added comments

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -8,11 +8,21 @@ namespace Mobjaan\Base;
 
 class Enqueue {
 
+    /**
+     * The bare bone function to add wordpress actions, hooks or filters
+     * @param 
+     * @return 
+     */
     function register() {
         add_action( 'admin_enqueue_scripts', array($this, 'enqueue_admin_assets') );
     }
 
 
+    /**
+     * Admin Styles and scripts
+     * @param 
+     * @return 
+     */
     function enqueue_admin_assets() {
         wp_enqueue_style('mobjaanpluginstyle', PLUGIN_URL . 'assets/css/main.css');
         wp_enqueue_script('mobjaanpluginscript', PLUGIN_URL . 'assets/js/main.js');
