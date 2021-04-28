0b7d4ee9d0e8e28799029ca2990308fa03ea8db2
Abdul Rehman <mehars.6925@gmail.com>
2021-04-28 18:30:54 +0500

    made it clean

--- mobjaan-plugin.php
+++ mobjaan-plugin.php
@@ -1,6 +1,6 @@
 <?php 
 /**
- * @package Mobjaan
+ * @package MobjaanPlugin
  */
 /*
 Plugin Name: Mobjaan Plugin
@@ -15,52 +15,16 @@ Text Domain: mobjaan-plugin
 
 defined('ABSPATH') or die('You can\'t access the file.');
 
-
 $autoload = dirname(__FILE__). '/vendor/autoload.php';
-
 if (!file_exists($autoload)) {  
     die ('Problem with Composer Autoload');
 } 
-
 require_once $autoload;
 
-use Mobjaan\Admin\Dashboard as AdminDashboard;
+define('PLUGIN_NAME', plugin_basename(__FILE__));
+define('PLUGIN_PATH', plugin_dir_path( __FILE__ ));
+define('PLUGIN_URL', plugin_dir_url( __FILE__ ));
 
-if ( !class_exists('MobjaanPlugin') ) {
-
-
-    class MobjaanPlugin 
-    {
-        private $admin;
-
-        function __construct() {
-            $this->admin = new AdminDashboard();
-            add_action( 'init', array( $this->admin, 'custom_post_type' )  );
-
-            // Register Admin Scripts
-            add_action( 'admin_enqueue_scripts', array($this->admin, 'enqueue_admin_assets') );
-            add_action( 'admin_menu', array($this->admin, 'add_admin_pages') );
-            add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), array($this->admin, 'plugin_link_filter') );
-        }
-
-        function activate() {
-            $this->admin->custom_post_type();
-            flush_rewrite_rules();
-        }
-
-        function deactivate() {
-            flush_rewrite_rules();
-        }
-
-        static function uninstall() {
-
-        }
-    }
-
-
-    
-    $mobjaan_plugin =  new MobjaanPlugin();
-    register_activation_hook( __FILE__, array( $mobjaan_plugin, 'activate' ) );
-    register_deactivation_hook( __FILE__, array( $mobjaan_plugin, 'deactivate' ) );
-    register_uninstall_hook( __FILE__, array( 'MobjaanPlugin' , 'uninstall' ) );
+if (class_exists('Mobjaan\\Init')) {
+    Mobjaan\Init::register_services();
 }
