a4ac0590a4b492eb627083fa7dcb26b81414e5cd
Abdul Rehman <mehars.6925@gmail.com>
2021-04-28 17:16:03 +0500

    composer autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f942bbb45993fd4a183b45cb304722ef0f6d2001',
+    'name' => 'abdul_rehman/mobjaan-plugin',
+  ),
+  'versions' => 
+  array (
+    'abdul_rehman/mobjaan-plugin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f942bbb45993fd4a183b45cb304722ef0f6d2001',
+    ),
+  ),
+);
