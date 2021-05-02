d11805f590fe138512064cfb9171104e75f2c0d3
Abdul Rehman <mehars.6925@gmail.com>
2021-05-02 13:26:50 +0500

    enqueued bootstrap

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -18,6 +18,7 @@ class Enqueue
     function register() 
     {
         add_action( 'admin_enqueue_scripts', array($this, 'enqueue_admin_assets') );
+        add_action( 'wp_enqueue_scripts', array($this, 'enqueue_assets') );
     }
 
 
@@ -30,6 +31,16 @@ class Enqueue
     {
         wp_enqueue_style('mobjaanpluginstyle', Constants::getPluginURL() . 'assets/css/main.css');
         wp_enqueue_script('mobjaanpluginscript', Constants::getPluginURL() . 'assets/js/main.js');
+    }
+
+    /**
+     * Standard Styles and scripts
+     * @param 
+     * @return 
+     */
+    function enqueue_assets() 
+    {
+        wp_enqueue_style('mobjaanpluginstyle_bootstrap', Constants::getPluginURL() . 'assets/css/bootstrap.min.css');
     }
 
 }
