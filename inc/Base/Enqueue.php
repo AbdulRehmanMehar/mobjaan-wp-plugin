de3501ed48ed0d20c6a986a690a410b7d86cc345
Abdul Rehman <mehars.6925@gmail.com>
2021-05-06 10:12:50 +0500

    updated

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -47,13 +47,21 @@ class Enqueue
      */
     function enqueue_assets() 
     {
+        
+        wp_enqueue_style('testimonial-css', Constants::getPluginURL() . 'assets/css/testimonials.css', array(), '1.0.0');
         wp_enqueue_style('mobjaanpluginstyle_bootstrap', Constants::getPluginURL() . 'assets/css/bootstrap.min.css');
         wp_enqueue_style('mobjaanpluginstyle_ratingcss', Constants::getPluginURL() . 'assets/css/rating.css');
         wp_enqueue_style('mobjaanpluginstyle_customcss', Constants::getPluginURL() . 'assets/css/client.css');
-        wp_enqueue_style('mobjaanpluginstyle_testimonialscss', Constants::getPluginURL() . 'assets/css/testimonials.css');
         wp_enqueue_script('jquery_custom', 'https://code.jquery.com/jquery-3.6.0.min.js');
         wp_enqueue_script('popperjs', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js');
         wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js');
+    
+        wp_enqueue_style( 
+            'font-awesome-5', 
+            'https://use.fontawesome.com/releases/v5.3.0/css/all.css', 
+            array(), 
+            '5.3.0' 
+        );
     }
 
 }
