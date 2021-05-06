a38286773e14c39226ebf76d70168e7f628d701e
Abdul Rehman <mehars.6925@gmail.com>
2021-05-06 07:40:58 +0500

    enqueued scripts

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -30,7 +30,14 @@ class Enqueue
     function enqueue_admin_assets() 
     {
         wp_enqueue_style('mobjaanpluginstyle', Constants::getPluginURL() . 'assets/css/main.css');
+        wp_enqueue_style( 
+            'font-awesome-5', 
+            'https://use.fontawesome.com/releases/v5.3.0/css/all.css', 
+            array(), 
+            '5.3.0' 
+        );
         wp_enqueue_script('mobjaanpluginscript', Constants::getPluginURL() . 'assets/js/main.js');
+        
     }
 
     /**
@@ -43,6 +50,10 @@ class Enqueue
         wp_enqueue_style('mobjaanpluginstyle_bootstrap', Constants::getPluginURL() . 'assets/css/bootstrap.min.css');
         wp_enqueue_style('mobjaanpluginstyle_ratingcss', Constants::getPluginURL() . 'assets/css/rating.css');
         wp_enqueue_style('mobjaanpluginstyle_customcss', Constants::getPluginURL() . 'assets/css/client.css');
+        wp_enqueue_style('mobjaanpluginstyle_testimonialscss', Constants::getPluginURL() . 'assets/css/testimonials.css');
+        wp_enqueue_script('jquery_custom', 'https://code.jquery.com/jquery-3.6.0.min.js');
+        wp_enqueue_script('popperjs', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js');
+        wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js');
     }
 
 }
